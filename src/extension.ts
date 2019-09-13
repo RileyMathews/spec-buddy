f3b11778f1a58472b3202568649ff16ffc9c9942
Riley Mathews <riley.mathews@daveramsey.com>
2019-09-13 11:13:39 -0500

    [clean-up-initial-code] moved code into app specific class

--- src/extension.ts
+++ src/extension.ts
@@ -1,37 +1,8 @@
 import * as vscode from 'vscode';
+import SpecBuddy from './spec_buddy/spec_buddy';
 
 export function activate(context: vscode.ExtensionContext) {
-	vscode.workspace.onDidOpenTextDocument((document) => {
-		const document_path = document.fileName;
-		if (document_path.substring(document_path.length - 3) !== ".rb" || document_path.includes("spec")) {
-			return;
-		}
-
-		if (vscode.workspace.getConfiguration('specBuddy').get('active') === false) {
-			return;
-		}
-
-		const root = vscode.workspace.workspaceFolders || [];
-		const root_path = root[0].uri.path;
-		const root_relative_path = document_path.replace(root_path, "");
-		const spec_file_path = `${root_path}/spec${root_relative_path.replace('.rb', '_spec.rb')}`;
-		vscode.workspace.openTextDocument(vscode.Uri.file(spec_file_path)).then(doc => {
-			vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside, true);
-		});
-	});
-
-	let turnOn = vscode.commands.registerCommand('specBuddy.on', () => {
-		const config = vscode.workspace.getConfiguration('specBuddy');
-		config.update('active', true, true);
-	});
-
-	let turnOff = vscode.commands.registerCommand('specBuddy.off', () => {
-		const config = vscode.workspace.getConfiguration('specBuddy');
-		config.update('active', false, true);
-	});
-
-	context.subscriptions.push(turnOn);
-	context.subscriptions.push(turnOff);
+	const _app = new SpecBuddy(context);
 }
 
 // this method is called when your extension is deactivated
---
+++ src/spec_buddy/spec_buddy.ts
@@ -0,0 +1,44 @@
+import * as vscode from 'vscode';
+
+export default class SpecBuddy {
+  public constructor(context: vscode.ExtensionContext) {
+    this.register_commands(context);
+    this.add_listener();
+  }
+
+  private add_listener() {
+    vscode.workspace.onDidOpenTextDocument((document) => {
+      const document_path = document.fileName;
+      if (document_path.substring(document_path.length - 3) !== ".rb" || document_path.includes("spec")) {
+        return;
+      }
+  
+      if (vscode.workspace.getConfiguration('specBuddy').get('active') === false) {
+        return;
+      }
+  
+      const root = vscode.workspace.workspaceFolders || [];
+      const root_path = root[0].uri.path;
+      const root_relative_path = document_path.replace(root_path, "");
+      const spec_file_path = `${root_path}/spec${root_relative_path.replace('.rb', '_spec.rb')}`;
+      vscode.workspace.openTextDocument(vscode.Uri.file(spec_file_path)).then(doc => {
+        vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside, true);
+      });
+    });
+  }
+
+  private register_commands(context: vscode.ExtensionContext) {
+    const turnOn = vscode.commands.registerCommand('specBuddy.on', () => {
+      const config = vscode.workspace.getConfiguration('specBuddy');
+      config.update('active', true, true);
+    });
+  
+    const turnOff = vscode.commands.registerCommand('specBuddy.off', () => {
+      const config = vscode.workspace.getConfiguration('specBuddy');
+      config.update('active', false, true);
+    });
+  
+    context.subscriptions.push(turnOn);
+    context.subscriptions.push(turnOff);
+  }
+}

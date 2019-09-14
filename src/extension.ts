b2042296fcf052a15c24d26911a09926179efdbe
Riley Mathews <rileymathews80@gmail.com>
2019-09-13 21:19:15 -0500

    refactored to open spec command

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,13 @@
 import * as vscode from 'vscode';
 import SpecBuddy from './spec_buddy/spec_buddy';
+import { appendFile } from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
-	const _app = new SpecBuddy(context);
+	const app = new SpecBuddy();
+
+	const openSpec = vscode.commands.registerCommand('specBuddy.openSpec', () => {
+		app.openSpec();
+	});
 }
 
 // this method is called when your extension is deactivated
--- src/spec_buddy/spec_buddy.ts
+++ src/spec_buddy/spec_buddy.ts
@@ -1,25 +1,17 @@
 import * as vscode from 'vscode';
 
 export default class SpecBuddy {
-  public constructor(context: vscode.ExtensionContext) {
-    this.register_commands(context);
-    this.add_listener();
-  }
-
-  private add_listener() {
-    vscode.workspace.onDidOpenTextDocument(this.file_open_callback);
-  }
-
-  private file_open_callback(document: vscode.TextDocument) {
-    if (vscode.workspace.getConfiguration('specBuddy').get('active') === false) {
+  public openSpec() {
+    const workspace = vscode.window.activeTextEditor;
+    if (workspace === undefined) {
       return;
     }
 
+    this.openSpecFile(workspace.document);
+  }
+
+  private openSpecFile(document: vscode.TextDocument) {
     const document_path = document.fileName;
-    if (document_path.substring(document_path.length - 3) !== ".rb" || document_path.includes("spec")) {
-      return;
-    }
-
     const root = vscode.workspace.workspaceFolders || [];
     const root_path = root[0].uri.path;
     const root_relative_path = document_path.replace(root_path, "");
@@ -27,20 +19,5 @@ export default class SpecBuddy {
     vscode.workspace.openTextDocument(vscode.Uri.file(spec_file_path)).then(doc => {
       vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside, true);
     });
-  }
-
-  private register_commands(context: vscode.ExtensionContext) {
-    const turnOn = vscode.commands.registerCommand('specBuddy.on', () => {
-      const config = vscode.workspace.getConfiguration('specBuddy');
-      config.update('active', true, true);
-    });
-  
-    const turnOff = vscode.commands.registerCommand('specBuddy.off', () => {
-      const config = vscode.workspace.getConfiguration('specBuddy');
-      config.update('active', false, true);
-    });
-  
-    context.subscriptions.push(turnOn);
-    context.subscriptions.push(turnOff);
   }
 }

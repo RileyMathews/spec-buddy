6cfdd89b8d6b5489d60331cd259c647b7cfb3177
Riley Mathews <rileymathews80@gmail.com>
2019-09-13 21:47:41 -0500

    refactored to use directory search

--- src/spec_buddy/spec_buddy.ts
+++ src/spec_buddy/spec_buddy.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import path = require('path');
 
 export default class SpecBuddy {
   public openSpec() {
@@ -11,13 +12,11 @@ export default class SpecBuddy {
   }
 
   private openSpecFile(document: vscode.TextDocument) {
-    const document_path = document.fileName;
-    const root = vscode.workspace.workspaceFolders || [];
-    const root_path = root[0].uri.path;
-    const root_relative_path = document_path.replace(root_path, "");
-    const spec_file_path = `${root_path}/spec${root_relative_path.replace('.rb', '_spec.rb')}`;
-    vscode.workspace.openTextDocument(vscode.Uri.file(spec_file_path)).then(doc => {
-      vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside, true);
+    const file_name = path.basename(document.fileName, path.extname(document.fileName));
+    vscode.workspace.findFiles(`{spec,specs,test,tests}/**/*${file_name}*`).then(result => {
+      vscode.workspace.openTextDocument(vscode.Uri.file(result[0].path)).then(doc => {
+        vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside, true);
+      });
     });
   }
 }
